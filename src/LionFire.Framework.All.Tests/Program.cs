3a10c762e3e40000a21b55dfb012a56cf9347a9e
Jared Thirsk <ja+commits@thirsk.ca>
2018-01-19 14:49:58 -0700

    snapshot

---
+++ src/LionFire.Framework.All.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LionFire.Framework.All.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
